6c1dba24937f0c1bf217d9f062a2b655da3654b9
Ricardo Ferreira <ricardo.ferreira@rackspace.com>
2018-02-05 10:08:34 +0000

    adding basic list structures

--- Integers/integers list multiplicationv2.py
+++ Integers/integers list multiplicationv2.py
@@ -1,14 +1,32 @@
 import numpy as np
+import time
+import random
+
 alist = [1, 7, 3, 1]
 
 def get_product_all(alist):
-    if len(alist)<2:
+    if len(alist) < 2:
         raise IndexError("to multiply we require at least 2")
+    start = time.time()
     alist.sort(reverse=True)
-    print(np.prod(np.array(alist[:3])))
+    value = np.prod(np.array(alist[:3]))
+    stop = time.time()
+    timevalue=stop-start
+    print("Sum is %d required %10.7f seconds "% (value, timevalue))
 
 def main():
-    get_product_all(alist)
+    i=10
+    while i<1000000000:
+        randomlist = random.sample(xrange(1000000000),i)
+        get_product_all(randomlist)
+        print "size"
+        print len(randomlist)
+        i*=10
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
