1ef121bb74c15206175c87d57ab9a7814f1b0497
Ricardo Ferreira <ricardo.ferreira@rackspace.com>
2018-02-09 09:11:04 +0000

    first of many hopefully

---
+++ reler/1.py
@@ -0,0 +1,16 @@
+def multiple(number_up_to):
+    result = 0
+    for i in range(1, number_up_to):
+        if i % 3 == 0 or i % 5 == 0:
+            result += i
+    print "the result is :"+str(result)
+    return result
+
+
+
+def main():
+    multiple(1000)
+
+
+if __name__ == "__main__":
+    main()
